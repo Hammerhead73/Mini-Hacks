1d2abb5b6d2c447128e676ff8b646bd694eebe61
Hammerhead <framinosona@hotmail.fr>
2016-10-02 14:22:41 +0200

    SimpleFinish

--- Xamarin/TwitterClient/TwitterClient/MainPage.xaml.cs
+++ Xamarin/TwitterClient/TwitterClient/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using TwitterAPI;
+using TwitterAPI.Abstractions;
 using Xamarin.Forms;
 
 namespace TwitterClient
@@ -11,5 +13,27 @@ namespace TwitterClient
             InitializeComponent();
         }
 
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			TweetsSearchBar.Text = "Cellenza";
+			UserSearchBar.Text = "Msft";
+			RefreshSearch(this, EventArgs.Empty);
+			RefreshUsers(this, EventArgs.Empty);
+		}
+
+		private async void RefreshSearch(object sender, EventArgs e)
+		{
+			TweetsList.IsRefreshing = true;
+			TweetsList.ItemsSource = await TwitterAPI.TwitterApi.Instance.SearchTweetsAsync(TweetsSearchBar.Text, 30, ResultType.Recent);
+			TweetsList.IsRefreshing = false;
+		}
+
+		private async void RefreshUsers(object sender, EventArgs e)
+		{
+			UsersList.IsRefreshing = true;
+			UsersList.ItemsSource = await TwitterAPI.TwitterApi.Instance.SearchUserAsync(UserSearchBar.Text, 30);
+			UsersList.IsRefreshing = false;
+		}
     }
 }
--- Xamarin/TwitterClient/TwitterClient/TwitterCredentials.cs
+++ Xamarin/TwitterClient/TwitterClient/TwitterCredentials.cs
@@ -12,9 +12,9 @@ namespace TwitterClient
         /// <summary>
         /// Get your twitter credentials here : https://apps.twitter.com/
         /// </summary>
-        public static string OAuthAccessToken = "Your OAuth Access Token";
-        public static string OAuthAccessTokenSecret = "Your OAuth Access Token Secret";
-        public static string ConsumerKey = "Your Consumer Key";
-        public static string ConsumerSecret = "Your Consumer Secret";
+        public static string OAuthAccessToken = "186907331-kNmekEJzTARcXc5k4PHOHqau2qQCs8tsh06O2xsf";
+        public static string OAuthAccessTokenSecret = "svA4A2MoxRphD5ZLkP5zZNjyHacBnFyqgFtLTBaaApuzn";
+        public static string ConsumerKey = "lpvvIVJbtT3CMn0vNdXJ6yiVO";
+        public static string ConsumerSecret = "TcGPQcBZ685QQUtdR7BlFohWyFT63QyAX0msLye3lP9ktP67rn";
     }
 }
